a1cabe9bf1511c521509df45d2c263a26e9002d0
NIKHITHA2006 <nimmicdm@gmail.com>
2026-03-23 22:26:15 +0530

    test companies route

--- backend/routes/companyRoutes.js
+++ backend/routes/companyRoutes.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { getCompanies, getCompanyById, createCompany, updateCompany, deleteCompany } = require('../controllers/companyController');
 
-// TEMP TEST ROUTE (to check if API works)
-router.get('/', (req, res) => {
-  res.json({ message: "Route working" });
-});
-
-// You can keep these for later (commented for now)
-// const { getCompanies, getCompanyById, createCompany, updateCompany, deleteCompany } = require('../controllers/companyController');
-
-// router.route('/').get(getCompanies).post(createCompany);
-// router.route('/:id').get(getCompanyById).put(updateCompany).delete(deleteCompany);
+router.route('/').get(getCompanies).post(createCompany);
+router.route('/:id').get(getCompanyById).put(updateCompany).delete(deleteCompany);
 
 module.exports = router;
